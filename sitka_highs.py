a2094655d864289a1998a4b681ab91787b3903ba
wataru5354 <setsunan76253@gmail.com>
2022-04-15 11:34:31 +0900

    最高気温のグラフの表示完了

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,7 +1,27 @@
 import csv
+from shutil import which
+import matplotlib.pyplot as plt 
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
   reader = csv.reader(f)
   header_row = next(reader)
-  print(header_row)
+
+  # ファイルから最高気温を取得する
+  highs = []
+  for row in reader:
+    high = int(row[5])
+    highs.append(high)
+
+# 最高気温のグラフを描画する
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs,c='red')
+
+# グラフにフォーマットを指定する
+plt.title("Daily high temperasures, July 2018",fontsize=24)
+plt.xlabel('',fontsize=16)
+plt.ylabel("Temperasure (F)",fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=16)
+
+plt.show()
